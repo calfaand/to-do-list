a10ec9f828a93fc97e3638ed14ec19003bc06c2b
Andrej Čalfa <45832809+calfaand@users.noreply.github.com>
2021-02-15 17:25:35 +0100

    upload files

---
+++ app.js
@@ -0,0 +1,34 @@
+const express= require("express");
+const bodyParser= require("body-parser");
+const app = express();
+app.set("view engine", "ejs");
+let items = ["Buy Food", "Eat Food", "Cook Food"];
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", function(req,res){
+    let today = new Date();
+    let options={
+        weekday:"long",
+        day: "numeric",
+        month: "long"
+    };
+    let day = today.toLocaleDateString("en-US", options);
+
+    res.render("list",{kindOfDay: day, newListItems : items});
+});
+
+app.post("/", function(req,res){
+    let item = req.body.newItem;
+    
+    items.push(item);
+
+    res.redirect("/");
+});
+
+
+app.listen(3000,function(){
+    console.log("listening on port 3000");
+
+});
